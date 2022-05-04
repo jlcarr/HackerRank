16075d06ea25a2c18a8b0b6b3a8d5afaf97be12c
Joseph Carr <joseph.carr@mail.mcgill.ca>
2022-05-04 19:09:33 -0400

    Solution to problem Ice Cream Parlor

---
+++ icecream-parlor/solution.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    price_map = {p:i for i,p in enumerate(arr)}
+    for i,c in enumerate(arr):
+        if m-c in price_map and i != price_map[m-c]:
+            return i+1, price_map[m-c]+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
+
